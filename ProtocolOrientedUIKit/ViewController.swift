f8a1de3dc5ab2732b33fc7b826b84f6792ce1475
IremTektas <tektasirem13@gmail.com>
2023-06-02 21:47:51 +0300

    Initial Commit

---
+++ ProtocolOrientedUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolOrientedUIKit
+//
+//  Created by iremt on 2.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
